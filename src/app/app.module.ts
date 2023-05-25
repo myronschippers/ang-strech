8cd733af791e60b84b7faaa2a1591f22484e2ddc
Myron R Schippers Jr <dubtwee@gmail.com>
2023-05-24 23:09:39 -0400

    stubbing in a heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
